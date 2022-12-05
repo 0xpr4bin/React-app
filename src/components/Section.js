71ec24412136c745cbb46c2a3d1f92327dc7d1a8
Prabin755421 <72201891+Prabin755421@users.noreply.github.com>
2022-12-05 17:12:46 +0545

    react

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import { Button } from './Button';
-import './HeroSection.css';
+import './Section.css';
 
 function HeroSection() {
   return (
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../HeroSection';
+import HeroSection from '../Section';
 import Footer from '../Footer';
 import Signup from './Signup';
 
