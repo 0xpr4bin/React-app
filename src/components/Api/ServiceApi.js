fe65090c2d0232beaaaa8d31ebc8dfb022dfde95
Prabin755421 <72201891+Prabin755421@users.noreply.github.com>
2022-12-14 14:58:47 +0545

    hbsjdjshd

---
+++ src/components/Api/ServiceApi.js
@@ -0,0 +1,40 @@
+
+export const serviceapi =[
+    {
+        id: 1,
+        logo: "fas fa-download",
+        title: "Register for free.",
+        info: "You can register for free and get started to buy online token for hospital,buses,plane,movie,etc",
+    },
+    {
+        id: 2,
+        logo: "fas fa-money-check-alt",
+        title: "Add recipient's bank details.",
+        info: "You can  add your recipient bank account number , branch, amount and bank name",
+    },
+    {
+        id: 3,
+        logo: "fab fa-amazon-pay ",
+        title: "Pay for the token.",
+        info: "You can pay according to the token you want to buy and settle the date as you like",
+    },
+    {
+        id: 4,
+        logo: "fas fa-user-check",
+        title: "Verify you identity",
+        info: "Please verify your identity via Login page to get start and enjoy our services ",
+    },
+    {
+        id: 5,
+        logo: "fas fa-hand-holding-usd ",
+        title: "Choose an amount to send",
+        info: "You can choose any amount ,want to send to the user from your bank account",
+    },
+    {
+        id: 6,
+        logo: "fas fa-globe",
+        title: "That's it",
+        info: "This website is an online token system for many platforms available",
+    }
+       
+]
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IconContext } from 'react-icons';
+import "../../App.css";
+import "../Api/ServiceApi";
+import { serviceapi } from '../Api/ServiceApi';
+import Footer from '../Footer';
 
 function Services() {
+  const [serviceData,setServiceData] =useState(serviceapi);  
   return (
-    <div className='services'>
-      <h1>Hello</h1>
-    </div>
+    <>
+    <IconContext.Provider value={{color:'blue'}}>
+      <section className='service-main-container'>
+        <div className='container service-container'>
+          <h1 className='main-heading text-center fw-bold'>How to buy online token?</h1>
+          <div className='row'>
+            {
+              serviceData.map((curElement)=>{
+                const{id,logo,title,info}=curElement;
+                return(
+                <>
+                    <div className='col-11 col-lg-4 col-xxl-4 work-container-subdiv' key={id}>
+                        <i  className={`fontawesome-style ${logo}`}></i>
+                        <a className='subheading'>{title}</a>
+                        <p className='subheading'>{info}</p>
+                   </div>
+                </>)
+
+              })
+            }
+            
+          </div>
+
+        </div>
+      </section>
+      </IconContext.Provider>
+
+      <Footer/>
+    </>
   )
 }
 
-export default Services
+export default Services;
